4206efd6d68beb0890fd1094554f11201144e9ff
shobe8011 <shobe7047@gmail.com>
2024-03-18 10:13:04 +0900

    チャントゲームオーバーになるように調整

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -30,7 +30,7 @@ public class GameManager : MonoBehaviour
     }
 
     [SerializeField] private MoveWall _moveWall;
-    [SerializeField] private GameOver _gameOver;
+    //[SerializeField] private GameOver _gameOver;
     [SerializeField] private Camera _MainCamera;
     [SerializeField] private Camera _TopCamera;
     [SerializeField] private UIViewer _UIViewer;
@@ -79,7 +79,7 @@ public class GameManager : MonoBehaviour
             // フルーツをセット
             case GameState.Set:
             {
-                _gameOver.SetGameOverFlag(true);
+                //_gameOver.SetGameOverFlag(true);
                 // フルーツをセット
                 _nextFruit = await _spawnFruits.SetNextFruit(_cancelToken);
                 if (_nextFruit != null)
@@ -104,7 +104,7 @@ public class GameManager : MonoBehaviour
                 }
 
                 _canFall = false;
-                _gameOver.SetGameOverFlag(false);
+                //_gameOver.SetGameOverFlag(false);
                 await _moveFruit.FallFruit(_nextFruit, _cancelToken);
                 _nextFruit = null;
                 // GameOverになっている可能性をチェック
